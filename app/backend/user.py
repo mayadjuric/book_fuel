c5487ce8e9671cb1b71d5e92c411f82b429d4dfd
mayadjuric <mayadaniella222@gmail.com>
2026-02-20 15:29:45 -0600

    created user class

---
+++ app/backend/user.py
@@ -0,0 +1,27 @@
+
+class User:
+
+    def __init__(self, username, study_year, total_burnout):
+        self.username = username
+        self.study_year = study_year
+        self.total_burnout = total_burnout
+
+
+
+    def get_username():
+        return self.username
+    
+    def get_study_year():
+        return self.study_year
+    
+    def get_total_burnout():
+        return self.total_burnout
+    
+    
+
+
+
+        
+
+    
+
