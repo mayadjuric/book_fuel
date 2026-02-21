cc6267c0e75c2d8c293d921436cff4fd7351df35
mayadjuric <mayadaniella222@gmail.com>
2026-02-20 20:43:07 -0600

    added blueprint for evaluations

--- app/backend/app.py
+++ app/backend/app.py
@@ -3,10 +3,13 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 from api.profile.user_api import user_blueprint
+from api.evaluations.evaluations_api import evaluation_blueprint
 
 app = flask.Flask(__name__)
 app.register_blueprint(user_blueprint)
 load_dotenv()
+
+app.register_blueprint(evaluation_blueprint)
 
 supabase: Client = create_client(
     os.environ.get("SUPABASE_URL"),
